edfbdc43626178842a0a1ca9f3ce16c037349d06
chr <chr+git@we3b2.org>
2013-09-21 11:47:22 +0200

    Emoticons etc

--- chat.php
+++ chat.php
@@ -33,10 +33,10 @@ if ( $_COOKIE['time'] == '' ) {
 	$id_last = $_COOKIE['time'];
 }
 
-$str = $_POST['msg'];
-$name = $_POST['name'];
-
 if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	$str = $_POST['msg'];
+	$name = $_POST['name'];
+
 	$txt = array('time' => $now, 'name' => $name, 'msg' => $str);
 	$results[] = $txt;
 	file_put_contents($chat_file, json_encode($results), LOCK_EX);
@@ -46,19 +46,32 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 	echo json_encode(array_slice($results,$i));
 
 	setcookie('time', $x['time'], time()+2592000);
+} elseif ( isset($_GET['sticker']) ) {
+	$str = $_GET['sticker'];
+	$name = $_GET['name'];
+
+	$txt = array('time' => $now, 'name' => $name, 'msg' => $str);
+	$results[] = $txt;
+	file_put_contents($chat_file, json_encode($results), LOCK_EX);
 } else {
+
 $html = "
 <html>
 <head>
-	<script
-		src='http://code.jquery.com/jquery-1.10.2.min.js'>
-	</script>
+	<link rel='stylesheet' href='http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css' />
+	<script src='http://code.jquery.com/jquery-1.10.2.min.js'></script>
+	<script src='http://code.jquery.com/ui/1.10.3/jquery-ui.min.js'></script>
+
 <script>
 
 // http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
 function replaceURLWithHTMLLinks(text) {
-	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-	return text.replace(exp,\"<a href='$1'>$1</a>\"); 
+	if ( text.search(/^<img/) == 0 ) {
+		return text;
+	} else {
+		var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+		return text.replace(exp,\"<a href='$1'>$1</a>\");
+	}
 }
 
 // http://www.quirksmode.org/js/cookies.html
@@ -98,8 +111,9 @@ setInterval(function(){
 		success: function(data){
 			if ( data[0]['time'] ) {
 				$.each(data, function(i, item) {
-					$('#chatbox').append( '<strong>' + item.name + '</strong>: ' );
-					$('#chatbox').append( replaceURLWithHTMLLinks(item.msg) + '<br />' );
+					var d = new Date(item.time * 1000);
+					var date = d.toLocaleTimeString(); // or toLocaleString()
+					$('#chatbox').append( '<p title=\'' + d + '\'><strong>' + item.name + '</strong>: <span style=\'font-size: 70%;\'>' + date + '</span><br />' + replaceURLWithHTMLLinks(item.msg) );
 				});
 // http://stackoverflow.com/questions/9830886/jquery-blinking-title
 if ( interv == 0 ) {
@@ -111,8 +125,6 @@ if ( interv == 0 ) {
 	// http://stackoverflow.com/questions/1966784/auto-scroll-to-bottom-of-page-with-jquery
 	$(document).scrollTop($(document).height());
 }, 1000);
-
-$
 
 $(function() {
 	$('#chatref').submit(function(event) {
@@ -154,20 +166,95 @@ $(function() {
 		document.title='chat';
 	});
 
+$(function() {
+	$('#stickers div a').click(function(event) {
+
+		var sticker = $(this);
+
+		$.ajax({
+			url: 'chat.php',
+			data: { name: user, sticker: sticker.html() }
+		}).fail(function() {
+			alert('FAIL');
+		});
+	      $('#stickers').toggle('drop');
+		event.preventDefault(); // Prevent submission via browser.
+	});
+	
+});
+
+$(function() {
+	$('#buttonEmoticon').click(function() {
+		// $('#stickers').css({ 'position': 'relative', 'bottom': this.height() });
+	      $('#stickers').toggle('drop');
+		event.preventDefault();
+	});
+
+});
+
+$(function() {
+	$( '#stickers' ).tabs({
+		event: 'mouseover'
+	});
+});
+
 </script>
 
 </head>
+
 <body style='z-index: 100; width: 100%; height: 90%;'>
 <div id='chatbox'></div>
 <form id='chatref'>
 <label id='chatlabel'></label>
 <input type='text' name='msg' id='msg' style='width: 80%;'></input>
 <button>!</button>
+<a href='#' id='buttonEmoticon'>Emoticon</a>
 </form>
+
+<div id='stickers' style='display: none'>
+  <ul>
+    <li><a href='#stickers-1'>emoticons 1</a></li>
+    <li><a href='#stickers-2'>emoticons 2</a></li>
+  </ul>
+<div id='stickers0'>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-57-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-14-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-54-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/cool-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/crying-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-59-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/in-love-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-12-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/guestion-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-31-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-16-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-3-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-18-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-27-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-48-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/emoticon-4-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/lol-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/private-3-xl.png'></a>
+<!--
+	<a href='#'><img src=''></a>
+-->
+</div>
+<div id='stickers-2'>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/sombrero-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/facebook-like-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/heart-69-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/pig-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/dog-3-xl.png'></a>
+	<a href='#'><img src='http://www.iconsdb.com/icons/preview/soylent-red/chili-pepper-13-xl.png'></a>
+<!--
+	<a href='#'><img src=''></a>
+-->
+</div>
+</div>
 </body>
 </html>
 ";
 	print($html);
 }
-
 ?>
